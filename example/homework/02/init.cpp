91f65418c9ce65e88c7c205bfd01b50c8249855a
Eddie Gannod <epgannod@gmail.com>
2024-03-02 16:57:17 -0600

    commit

--- example/homework/02/init.cpp
+++ example/homework/02/init.cpp
@@ -13,6 +13,7 @@ int main(int argc, char* argv[]) {
   Kokkos::View<int*> prob_2("prob_2", n);
   // set values to 1000 * i * j;
   Kokkos::parallel_for("iterator", n, KOKKOS_LAMBDA(const int& i) {
+    std::cout << i << std::endl;
     prob_2(i) = i;
   });
   std::cout << prob_2(1) << std::endl;
