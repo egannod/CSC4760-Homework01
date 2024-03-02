be9bc96744eb2fa86a7ef8f4b926a07bd599f1f7
Eddie Gannod <epgannod@gmail.com>
2024-03-02 17:06:16 -0600

    commit

--- example/homework/02/init.cpp
+++ example/homework/02/init.cpp
@@ -19,9 +19,7 @@ int main(int argc, char* argv[]) {
   for (int i=0; i<n; i++){
     prob_2(i) = 1000 * i;
   }
-  for (int i=0; i<n; i++){
-    std::cout << prob_2(i) << std::endl;
-  }
+  std::cout << prob_2(4) << std::endl;
   }
   Kokkos::finalize();
 }
