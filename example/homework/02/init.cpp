8cb447612530b865c439e75b2aaee467cbeb9245
Eddie Gannod <epgannod@gmail.com>
2024-03-02 16:56:33 -0600

    commit

--- example/homework/02/init.cpp
+++ example/homework/02/init.cpp
@@ -5,16 +5,18 @@
 // Problem: Make an n ∗ m View where each index equals 1000 ∗ i ∗ j
 
 int main(int argc, char* argv[]) {
-  Kokkos::initialize(/*argc, argv*/);
+  Kokkos::initialize(argc, argv);
+  {
   // set n and m, you can change these values
   int n,m = 4;
   // Make View
   Kokkos::View<int*> prob_2("prob_2", n);
   // set values to 1000 * i * j;
   Kokkos::parallel_for("iterator", n, KOKKOS_LAMBDA(const int& i) {
-    prob_2(i) = i * i;
+    prob_2(i) = i;
   });
   std::cout << prob_2(1) << std::endl;
+  }
 
   Kokkos::finalize();
 }
