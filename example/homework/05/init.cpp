d0e0683b14edcb207ba54a6163f2e65110b08890
Eddie Gannod <epgannod@gmail.com>
2024-03-05 16:09:28 -0600

    Commit for Problem 4

--- example/homework/05/init.cpp
+++ example/homework/05/init.cpp
@@ -7,11 +7,31 @@ int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
   // Make View and create values
- 
-  // sum loops 
+  Kokkos::View<int*> view_5("view_5", 10);
+  for(int i=0; i<view_5.extent(0); i++){
+    view_5(i) = 100*i;
+  }
+
+  // Sum Loops
+  int sum = 0;
+  int parallel_sum = 0;
+
+  Kokkos::Timer timer;
+  for(int i=0; i<view_5.extent(0); i++){
+    sum += view_5(i);
+  }
+  double time = timer.seconds();
+
+  timer.reset();
+  Kokkos::parallel_reduce("parallel sum", view_5.extent(0), KOKKOS_LAMBDA(const int& i, int& lsum){
+    lsum+=view_5(i);
+  }, parallel_sum);
+
+  double parallel_time = timer.seconds();
   
   // Output times
-
+  std::cout << "Sum: " << sum << " Time: " << time << std::endl;
+  std::cout << "Parallel Sum: " << parallel_sum << " Time: " << parallel_time << std::endl;
   }
   Kokkos::finalize();
 }
