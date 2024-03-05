28f2eb4e872277571ba032ab44103bdec612f6bb
Eddie Gannod <epgannod@gmail.com>
2024-03-05 15:55:15 -0600

    Commit for Problem 4

--- example/homework/04/init.cpp
+++ example/homework/04/init.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
   {
   // Make View and create values
   int lval;
-  Kokkos::View<int*> view_4("view_4", 8);
+  Kokkos::View<int*> view_4("view_4", 10);
 
   for(int i=0; i<view_4.extent(0); i++){
     view_4(i) = i*i;
@@ -17,7 +17,6 @@ int main(int argc, char* argv[]) {
   
   // Do a parallel reduction
   Kokkos::parallel_reduce("largest value", view_4.extent(0), KOKKOS_LAMBDA(const int& i, int& val){
-    std::cout << "view_4(" << i << ") = " << view_4(i) << ", val = " << val << std::endl;
     val = (view_4(i) > val) ? view_4(i) : val;
   }, Kokkos::Max<int>(lval));
   Kokkos::fence();
