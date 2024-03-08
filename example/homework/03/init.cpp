d5fff9b809bd6d1b9a484422806baf56c87e8f19
Eddie Gannod <epgannod@gmail.com>
2024-03-07 18:19:17 -0600

    Commit for Problem 3

--- example/homework/03/init.cpp
+++ example/homework/03/init.cpp
@@ -7,12 +7,15 @@
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
-    int n = 10;
+    int n;
+    std::cin >> n;
   // Make View
   Kokkos::View<int*[5][7][12]> view("view", n);
   
   // print name
   std::cout << "View label: " << view.label() << std::endl;
+  // print dimensions
+  std::cout << "View dimensions: " << view.extent(0) << "X" << view.extent(1) << "X" << view.extent(2) << "X" << view.extent(3) << std::endl;
   }
   Kokkos::finalize();
 }
